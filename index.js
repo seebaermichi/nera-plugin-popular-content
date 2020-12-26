e5d8c941b162a431c269ebd6b8a797c0cad872d2
Michael Becker <michael.becker@ueber.io>
2020-12-26 20:36:55 +0100

    Include content in popular content data

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 const { getConfig } = require('../plugin-helper')
 
 module.exports = (() => {
-  const config = getConfig(`${__dirname}/config/popular-content.yaml`)
+    const config = getConfig(`${__dirname}/config/popular-content.yaml`)
 
-  const getPopularContent = pagesData => {
-    const popularContent = {}
+    const getPopularContent = pagesData => {
+        const popularContent = {}
 
-    config.properties.forEach(({ meta_property_name, order }) => {
-      popularContent[meta_property_name] = []
+        config.properties.forEach(({ meta_property_name, order }) => {
+            popularContent[meta_property_name] = []
 
-      pagesData.forEach(({ meta }) => {
-        if (meta[meta_property_name]) {
-          popularContent[meta_property_name].push(meta)
-        }
-      })
+            pagesData.forEach(({ meta, content }) => {
+                if (meta[meta_property_name]) {
+                    meta.content = content
+                    popularContent[meta_property_name].push(meta)
+                }
+            })
 
-      popularContent[meta_property_name].sort((a, b) => {
-        if (order && order === 'desc') {
-          return b[meta_property_name] - a[meta_property_name]
-        }
-        return a[meta_property_name] - b[meta_property_name]
-      })
-    })
+            popularContent[meta_property_name].sort((a, b) => {
+                if (order && order === 'desc') {
+                    return b[meta_property_name] - a[meta_property_name]
+                }
+                return a[meta_property_name] - b[meta_property_name]
+            })
+        })
 
-    return popularContent
-  }
-
-  const getAppData = data => {
-    if (data.app !== null && typeof data.app === 'object') {
-      return Object.assign({}, data.app, {
-        popularContent: getPopularContent(data.pagesData)
-      })
+        return popularContent
     }
 
-    return data.app
-  }
+    const getAppData = data => {
+        if (data.app !== null && typeof data.app === 'object') {
+            return Object.assign({}, data.app, {
+                popularContent: getPopularContent(data.pagesData)
+            })
+        }
 
-  return {
-    getAppData
-  }
+        return data.app
+    }
+
+    return {
+        getAppData
+    }
 })()
